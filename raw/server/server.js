9c8df8b86ba67f0ec2a5f4c96065b8c34882e736
Jason Tan <jastan313@gmail.com>
2017-10-12 22:35:24 -0700

    Added index route. Can load index.html view!

--- raw/server/server.js
+++ raw/server/server.js
@@ -1,7 +1,7 @@
 // modules =================================================
 var express = require('express');
 var app = express();
-var router = express.Router();
+var path = require('path');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
@@ -20,7 +20,7 @@ var db = require('../config/db.js');
 var port = process.env.PORT || 8080; 
 
 // connect to our mongoDB database 
-mongoose.connect(db.url); 
+mongoose.connect(db.url);
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
@@ -35,7 +35,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(methodOverride('X-HTTP-Method-Override')); 
 
+app.use(express.static(path.join(__dirname, 'raw/client')));
+
 // routes ==================================================
+require('../app/routes/index_route')(app);
 require('../app/routes/users_route')(app);
 require('../app/routes/docs_route')(app);
 require('../app/routes/comments_route')(app); // configure our routes

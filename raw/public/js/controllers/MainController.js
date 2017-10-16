2eab93503e4ca7105559e21e24af5d64722f2638
Jason Tan <jastan313@gmail.com>
2017-10-15 21:28:06 -0700

    Minor changes. Still need to debug routes

--- raw/public/js/controllers/MainController.js
+++ raw/public/js/controllers/MainController.js
@@ -1,3 +1,3 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
+angular.module('MainCtrl', []).controller('MainController', function ($scope) {
     $scope.tagline = 'This is MainCtrl';   
 });

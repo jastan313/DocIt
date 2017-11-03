3522d5b8545a0a991a17eac1cd0b639f9c2ac988
Jason Tan <jastan313@gmail.com>
2017-11-02 23:13:26 -0700

    Completed docit directory menu and styling. Still need to check if directory menu requries css media queries. Renamed a few html IDs. Added basic navigation javascript for new buttons.

--- raw/public/js/navigation.js
+++ raw/public/js/navigation.js
@@ -1,13 +1,40 @@
 window.onload = function () {
-    var loginNavBtn = document.getElementById('login-navigation-btn');
-    var signupNavBtn = document.getElementById('signup-navigation-btn');
-    if (loginNavBtn) {
-        loginNavBtn.addEventListener('click', function () {
+    var loginToSignupBtn = document.getElementById('login-tosignup-btn');
+    var signupToLoginBtn = document.getElementById('signup-tologin-btn');
+    var docitDirBtn = document.getElementById('docit-directory-btn');
+    var docitDirectory = document.getElementById('docit-directory');
+    var docitDirSaveBtn = document.getElementById('docit-directory-save-btn');
+    var docitDirToDocboardBtn = document.getElementById('docit-directory-todocboard-btn');
+    var docitDirLogoutBtn = document.getElementById('docit-directory-logout-btn');
+    if (loginToSignupBtn) {
+        loginToSignupBtn.addEventListener('click', function () {
             window.location.href = '../views/signup_template.html';
         });
     }
-    if (signupNavBtn) {
-        signupNavBtn.addEventListener('click', function () {
+    if (signupToLoginBtn) {
+        signupToLoginBtn.addEventListener('click', function () {
+            window.location.href = '../views/login_template.html';
+        });
+    }
+    if (docitDirBtn) {
+        docitDirBtn.addEventListener('click', function () {
+            docitDirBtn.style.display = "none";
+            docitDirectory.style.display = "inherit";
+        });
+    }
+    if (docitDirSaveBtn) {
+        docitDirSaveBtn.addEventListener('click', function () {
+            docitDirBtn.style.display = "inherit";
+            docitDirectory.style.display = "none";
+        });
+    }
+    if (docitDirToDocboardBtn) {
+        docitDirToDocboardBtn.addEventListener('click', function () {
+            window.location.href = '../views/docboard_template.html';
+        });
+    }
+    if (docitDirLogoutBtn) {
+        docitDirLogoutBtn.addEventListener('click', function () {
             window.location.href = '../views/login_template.html';
         });
     }

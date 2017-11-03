1a791bc0eee03034ed677b90b6555b4bb33cd9db
Jason Tan <jastan313@gmail.com>
2017-11-03 00:00:20 -0700

    Added docview javascript navigation.

--- raw/public/js/navigation.js
+++ raw/public/js/navigation.js
@@ -2,17 +2,29 @@ window.onload = function () {
     var loginToSignupBtn = document.getElementById('login-tosignup-btn');
     var signupToLoginBtn = document.getElementById('signup-tologin-btn');
     var docContainer = document.getElementById('doc-container');
+    
     var docitDirBtn = document.getElementById('docit-directory-btn');
     var docitDirectory = document.getElementById('docit-directory');
     var docitDirSaveBtn = document.getElementById('docit-directory-save-btn');
     var docitDirToDocboardBtn = document.getElementById('docit-directory-todocboard-btn');
     var docitDirLogoutBtn = document.getElementById('docit-directory-logout-btn');
 
+    var docviewDirBtn = document.getElementById('docview-directory-btn');
+    var docviewDirectory = document.getElementById('docview-directory');
+    var docviewDirEditBtn = document.getElementById('docview-directory-edit-btn');
+    var docviewDirToDocboardBtn = document.getElementById('docview-directory-todocboard-btn');
+    var docviewDirLogoutBtn = document.getElementById('docview-directory-logout-btn');
+
     window.addEventListener('click', function (e) {
         if (docitDirBtn)
             docitDirBtn.style.display = "inherit";
         if (docitDirectory)
             docitDirectory.style.display = "none";
+
+        if (docviewDirBtn)
+            docviewDirBtn.style.display = "inherit";
+        if (docviewDirectory)
+            docviewDirectory.style.display = "none";
     });
     if (loginToSignupBtn) {
         loginToSignupBtn.addEventListener('click', function () {
@@ -24,6 +36,7 @@ window.onload = function () {
             window.location.href = '../views/login_template.html';
         });
     }
+    
     if (docitDirBtn) {
         docitDirBtn.addEventListener('click', function (e) {
             docitDirBtn.style.display = "none";
@@ -45,6 +58,31 @@ window.onload = function () {
     }
     if (docitDirLogoutBtn) {
         docitDirLogoutBtn.addEventListener('click', function () {
+            window.location.href = '../views/login_template.html';
+        });
+    }
+
+    if (docviewDirBtn) {
+        docviewDirBtn.addEventListener('click', function (e) {
+            docviewDirBtn.style.display = "none";
+            docviewDirectory.style.display = "inherit";
+            e.stopPropagation();
+        });
+    }
+    if (docviewDirEditBtn) {
+        docviewDirEditBtn.addEventListener('click', function (e) {
+            docviewDirBtn.style.display = "inherit";
+            docviewDirectory.style.display = "none";
+            e.stopPropagation();
+        });
+    }
+    if (docviewDirToDocboardBtn) {
+        docviewDirToDocboardBtn.addEventListener('click', function () {
+            window.location.href = '../views/docit_template.html';
+        });
+    }
+    if (docviewDirLogoutBtn) {
+        docviewDirLogoutBtn.addEventListener('click', function () {
             window.location.href = '../views/login_template.html';
         });
     }

36d8fc9f71cdd19ca16034942eb5f9a22955a590
Jason Tan <jastan313@gmail.com>
2017-11-24 23:27:39 -0800

    Added login attempts to Login controller. Changed login console logging to info popup.

--- public/js/controllers/LoginController.js
+++ public/js/controllers/LoginController.js
@@ -6,21 +6,38 @@ angular.module('LoginCtrl', []).controller('LoginControlller', function ($scope,
             User.getByAlias($scope.formAlias).then(function (user) {
                 if (user != null) {
                     if ($scope.formPassword === user.password) {
-                        Page.setUser(user);
-                        $scope.changePage('docboard');
+                        var userData = {"login_attempts": 0};
+                        User.update(user._id, userData).then(function (user) {
+                            Page.setUser(user);
+                            $scope.changePage('docboard');
+                        }, function (err) {
+                            console.log("Login: User Update Error:" + err);
+                        });
+                    } else {
+                        var loginAttempts = (user.login_attempts + 1) % 5;
+                        var userData = {"login_attempts": loginAttempts};
+                        User.update(user._id, userData).then(function (user) {
+                            if (loginAttempts === 0) {
+                                $scope.displayInfoPopup("Login Alias and password did not match. An email has been sent to " +
+                                        user.alias + "\'s email address |" + "| to recover the account.");
+                                //
+                            } else {
+                                $scope.displayInfoPopup("Login Alias and password did not match.\nNumber of attempts: " +
+                                        loginAttempts + ".\n\nNote: Upon reaching five unsuccessful attempts, a recovery email will be sent.");
+                            }
+
+                        }, function (err) {
+                            console.log("Login: User Update Error:" + err);
+                        });
                     }
-                    else {
-                        console.log("Login: Password Does Not Match Error.");
-                    }
-                }
-                else {
-                    console.log("Login: Alias Does Not Exist Error.");
+                } else {
+                    $scope.displayInfoPopup("Login Alias does not exist. Please signup by clicking the \"Create\" button.");
                 }
             }, function (err) {
-                console.log("Login: Submit Error:" + err);
+                console.log("Login: User Get Error:" + err);
             });
         } else {
-            console.log("Login: Validation Error.");
+            $scope.displayInfoPopup("Login credentials not valid. Please try again.");
         }
     }
 });

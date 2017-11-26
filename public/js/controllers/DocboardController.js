dc61ba92d18bb3ad07f1eca3d4effd13edc0ee50
Jason Tan <jastan313@gmail.com>
2017-11-25 23:57:01 -0800

    Added basic Help directory option to Docboard.

--- public/js/controllers/DocboardController.js
+++ public/js/controllers/DocboardController.js
@@ -97,4 +97,19 @@ angular.module('DocboardCtrl', []).controller('DocboardControlller', function ($
             console.log("Docboard: Go Doc Error: " + err);
         });
     }
+    
+    $scope.help = function() {
+        displayInfoPopup("Hi, " + user.alias + "! Welcome to |DOCIT|, a document-based web \
+        application where writers are encouraged to brainstorm, write, and publish any type \
+        of creative, text-based work anonymously. You may create new Docs, edit drafts until \\n\
+        you are satisfied, and publish their finalized form for public viewing and rating.\n\n\
+        |DOCBOARD|: Here is your main hub where you will find two sections, the Doc Archive \
+        and the Doc Feed.\nThe Doc Archive is a list of all your saved Docs as well as the option \
+        to start a new Doc draft. Creating a new Doc or selecting an existing Doc that has yet to \
+        be published will direct you to |DOCIT|. Selecting a published Doc will direct you to \
+        |DOCVIEW|. \nThe Doc Feed will show you recently published Docs by you and fellow writers, \
+        sorted by highest rating. Selecting any Doc in the Doc Feed will direct you to |DOCVIEW|.\n\n\
+        |DOCIT|: Using Docit, you will have the option to view, edit, save, export, and publish your \
+        Docs.\n\n|DOCVIEW|: Using Docview, you will be able to view and rate published-only Docs.");
+    }
 });

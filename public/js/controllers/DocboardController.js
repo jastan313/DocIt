eb99baa6b100d7a94dd66dbc28de923818e52fbe
Jason Tan <jastan313@gmail.com>
2017-11-30 22:13:02 -0800

    Debugged Docboard directory not showing.

--- public/js/controllers/DocboardController.js
+++ public/js/controllers/DocboardController.js
@@ -8,17 +8,13 @@ angular.module('DocboardCtrl', []).controller('DocboardController', function ($s
         $scope.directoryShow = !$scope.directoryShow;
     }
 
-    $scope.getDocs = function () {
+    $scope.getDocArchive = function () {
         var user = Page.getUser();
         if (user) {
             Doc.getByUserID(user._id).then(function (response) {
                 var docs = response.data;
                 for (var i = 0; i < docs.length; i++) {
                     var doc = Doc.formatDoc(docs[i]);
-                    var docID = doc._id;
-                    var docTitle = doc.title;
-                    var docAlias = doc.author.alias;
-                    var docDate = doc.date;
                     var docRating = "Not Published";
                     if (doc.published) {
                         if (doc.rating === 0) {
@@ -30,9 +26,9 @@ angular.module('DocboardCtrl', []).controller('DocboardController', function ($s
                         }
                     }
                     $scope.docArchive.push(
-                            {_id: docID,
-                                title: docTitle,
-                                date: docDate,
+                            {_id: doc._id,
+                                title: doc.title,
+                                date: doc.date,
                                 rating: docRating
                             }
                     );
@@ -49,10 +45,6 @@ angular.module('DocboardCtrl', []).controller('DocboardController', function ($s
             var docs = response.data;
             for (var i = 0; i < docs.length; i++) {
                 var doc = Doc.formatDoc(docs[i]);
-                var docID = doc._id;
-                var docTitle = doc.title;
-                var docAlias = doc.author.alias;
-                var docDate = doc.date;
                 var docRating = docs[i].rating;
                 if (docRating === 0) {
                     docRating = "0 Rating";
@@ -62,11 +54,12 @@ angular.module('DocboardCtrl', []).controller('DocboardController', function ($s
                     docRating = "-" + docRating + " Rating";
                 }
                 $scope.docFeed.push(
-                        {_id: docID,
-                            title: docTitle,
-                            alias: docAlias,
-                            date: docDate,
-                            rating: docRating}
+                        {_id: doc._id,
+                            title: doc.title,
+                            alias: doc.author.alias,
+                            date: doc.date,
+                            rating: docRating
+                        }
                 );
             }
             $scope.objToString($scope.docFeed);
@@ -81,33 +74,32 @@ angular.module('DocboardCtrl', []).controller('DocboardController', function ($s
             Doc.get(docID).then(function (response) {
                 Page.setDoc(response.data);
                 if (response.data.published) {
-                    $scope.changePage('docit');
-                } else {
                     $scope.changePage('docview');
+                } else {
+                    $scope.changePage('docit');
                 }
             });
         }
     }
 
     $scope.help = function () {
-        displayInfoPopup("Hi, " + user.alias + "! Welcome to |DOCIT|, a document-based web \
+        $scope.displayInfoPopup("Help",
+        "Hi, " + Page.getUser().alias + "! Welcome to |DOCIT|, a document-based web \
         application where writers are encouraged to brainstorm, write, and publish any type \
         of creative, text-based work anonymously. You may create new Docs, edit drafts until \n\
         you are satisfied, and publish their finalized form for public viewing and rating.\n\n\
         |DOCBOARD|: Here is your main hub where you will find two sections, the Doc Archive \
-        and the Doc Feed.\nThe Doc Archive is a list of all your saved Docs as well as the option \
+        and the Doc Feed.\n\nThe Doc Archive is a list of all your saved Docs as well as the option \
         to start a new Doc draft. Creating a new Doc or selecting an existing Doc that has yet to \
         be published will direct you to |DOCIT|. Selecting a published Doc will direct you to \
-        |DOCVIEW|. \nThe Doc Feed will show you recently published Docs by you and fellow writers, \
+        |DOCVIEW|.\n\nThe Doc Feed will show you recently published Docs by you and fellow writers, \
         sorted by highest rating. Selecting any Doc in the Doc Feed will direct you to |DOCVIEW|.\n\n\
         |DOCIT|: Using Docit, you will have the option to view, edit, save, export, and publish your \
         Docs.\n\n|DOCVIEW|: Using Docview, you will be able to view and rate published-only Docs.");
     }
 
     $scope.init = function () {
-        console.log('init');
-        $scope.getDocs();
+        $scope.getDocArchive();
         $scope.getDocFeed(10, 7);
-        console.log('init done');
     }
 });
--- public/js/controllers/SignupController.js
+++ public/js/controllers/SignupController.js
@@ -46,10 +46,10 @@ angular.module('SignupCtrl', []).controller('SignupController', function ($scope
                         of creative, text-based work anonymously. You may create new Docs, edit drafts until \n\
                         you are satisfied, and publish their finalized form for public viewing and rating.\n\n\
                         |DOCBOARD|: Here is your main hub where you will find two sections, the Doc Archive \
-                        and the Doc Feed.\nThe Doc Archive is a list of all your saved Docs as well as the option \
+                        and the Doc Feed.\n\nThe Doc Archive is a list of all your saved Docs as well as the option \
                         to start a new Doc draft. Creating a new Doc or selecting an existing Doc that has yet to \
                         be published will direct you to |DOCIT|. Selecting a published Doc will direct you to \
-                        |DOCVIEW|.\nThe Doc Feed will show you recently published Docs by you and fellow writers, \
+                        |DOCVIEW|.\n\nThe Doc Feed will show you recently published Docs by you and fellow writers, \
                         sorted by highest rating. Selecting any Doc in the Doc Feed will direct you to |DOCVIEW|.\n\n\
                         |DOCIT|: Using Docit, you will have the option to view, edit, save, export, and publish your \
                         Docs.\n\n|DOCVIEW|: Using Docview, you will be able to view and rate published-only Docs.");
